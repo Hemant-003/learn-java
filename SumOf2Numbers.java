bba748f54c88495b3a07b0d2e5597b86846b4291
Raj <rajkum@thoughtworks.com>
2017-08-05 23:42:45 +0530

    Adding program for finding sum of numbers

---
+++ SumOf2Numbers.java
@@ -0,0 +1,9 @@
+class SumOf2Numbers{
+	public static void main(String[] args) {
+		System.out.println("Sum of 2 numbers is " + add(4,5));
+	}
+
+	public static int add(int x, int y){
+		return x + y;
+	}
+}
