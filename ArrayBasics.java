f0d438adb93b594c5e1d86f5a36a26ef2019d282
Raj <rajkum@thoughtworks.com>
2017-08-06 23:20:57 +0530

    Basics of array

---
+++ ArrayBasics.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class ArrayBasics{
+	public static void main(String[] args) {
+		int uy;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter length of array");
+		int length = sc.nextInt();
+		int x[] = new int[length];
+		System.out.println(x.length);
+	}
+}
