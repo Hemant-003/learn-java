956f37139f0d70e1fb8f680a9581730fd03e4de3
Raj <rajkum@thoughtworks.com>
2017-08-14 22:48:37 +0530

    Just seeing how type casting works

---
+++ TypeCast.java
@@ -0,0 +1,19 @@
+class TypeCast {
+	public static void main(String[] args) {
+		int x = 5;
+		float y = 6.92f;
+		double z = 222223323234433d;
+
+		int xx = (int)z;
+		float yy = x;
+		double zz = y;
+
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println("-------");
+		System.out.println(xx);
+		System.out.println(yy);
+		System.out.println(zz);
+	}
+}
