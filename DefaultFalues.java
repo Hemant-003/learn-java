04fd2ab4d2810fdd0468b6a43add6b7bd4b60632
Raj <rajkum@thoughtworks.com>
2017-08-14 23:00:27 +0530

    Checking default values of different data types

---
+++ DefaultFalues.java
@@ -0,0 +1,23 @@
+class DefaultFalues {
+	static int x;
+	static String y;
+	static Dog z;
+	static char a;
+	static float f;
+	static boolean s;
+
+	public static void main(String[] args) {
+		
+		System.out.println(x);
+		
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println("dddd" + '\u0000' + "dddd");
+		System.out.println(f);
+		int[] myList = {5,8};
+	}
+
+	class Dog{
+
+	}
+}
